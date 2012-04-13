24e6ea3d5e730e2436d37bfe2c56a71403ab0a52
Mike Grissom <mikeyman77@gmail.com>
2012-04-13 09:10:57 -0700

    updated build fingerprint and description

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=samsung_hercules BUILD_FINGERPRINT="htc_europe/htc_shooteru/shooteru:2.3.4/GRJ22/105308.8:user/release-keys" PRIVATE_BUILD_DESC="1.20.401.8 CL105308 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=samsung_hercules BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.0.3/IML74K/107:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.0.3 IML74K 107 release-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-540x960
 
