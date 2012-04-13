aef90f0adf87cb90899ea7aa0d14e4e158d301a2
Mike Grissom <mikeyman77@gmail.com>
2012-04-13 09:24:12 -0700

    Revert "add ability to extract blobs from unzipped ROM"
    This reverts commit f0bb37f448b99b62d0750305d03d5bcb5b5b7b4d.

--- extract-files.sh
+++ extract-files.sh
@@ -1,41 +1,14 @@
 #!/bin/sh
 
-<<<<<<< HEAD
 BASE=../../../vendor/samsung/hercules/proprietary
-=======
-set -e
-
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
-fi
-
-BASE=../../../vendor/samsung/hercules/proprietary
->>>>>>> f90cec0... add ability to extract blobs from unzipped ROM
 rm -rf $BASE/*
 
 for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
-  echo "Extracting /system/$FILE ..."
   DIR=`dirname $FILE`
   if [ ! -d $BASE/$DIR ]; then
     mkdir -p $BASE/$DIR
   fi
-  if [ "$SRC" = "adb" ]; then
-    adb pull /system/$FILE $BASE/$FILE
-  else
-    cp $SRC/system/$FILE $BASE/$FILE
-  fi
+  adb pull /system/$FILE $BASE/$FILE
 done
 
 ./setup-makefiles.sh
