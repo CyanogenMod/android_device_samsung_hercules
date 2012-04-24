57ce904a91da303068b59c61d16bcd9716deaad8
David Ferguson <ferguson.david@gmail.com>
2012-04-23 18:12:55 -0700

    build lights.hercules.so

--- device.mk
+++ device.mk
@@ -40,9 +40,13 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/hercules/overlay
 PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
 
-# GPS and Light
+# GPS
 PRODUCT_PACKAGES += \
-    gps.hercules \
+    gps.hercules
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.hercules
 
 # Kernel and modules
 ifeq ($(TARGET_PREBUILT_KERNEL),)
