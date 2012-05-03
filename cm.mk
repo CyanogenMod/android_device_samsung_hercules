37d4458f9c6b7c77d75c95c2c3be096db7baab88
Mike Grissom <mikeyman77@gmail.com>
2012-05-02 23:18:59 -0700

    update fingerprint strings

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=samsung_hercules BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.0.3/IML74K/107:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.0.3 IML74K 107 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=samsung_hercules BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.0.3/IML74K/UVLC8:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.0.3 IML74K UVLC8 release-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-480x800
 
