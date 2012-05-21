6787269390410fe966cf1bda625ff3585f735c53
Mike Grissom <mikeyman77@gmail.com>
2012-05-21 12:26:50 -0700

    hercules: Fix include order
    Change-Id: I4737767900ba5a94d2a771beca52d0ed81038fe5

--- cm.mk
+++ cm.mk
@@ -1,10 +1,11 @@
 $(call inherit-product, device/samsung/hercules/full_hercules.mk)
 
 # Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=samsung_hercules BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.0.3/IML74K/UVLC8:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.0.3 IML74K UVLC8 release-keys"
 
