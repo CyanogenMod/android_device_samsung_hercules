1269a60a31b359e9880516eae5c51528fd4b4979
Mike Grissom <mikeyman77@gmail.com>
2012-05-21 12:56:15 -0700

    hercules: fix PRODUCT_NAME and override TARGET_DEVICE for build.prop
    Change-Id: I1ac61b57a5398beccebfe7576ac6d63f5d1afe9a

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=samsung_hercules BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.0.3/IML74K/UVLC8:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.0.3 IML74K UVLC8 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T989 TARGET_DEVICE=SGH-T989 BUILD_FINGERPRINT="samsung/SGH-T989/SGH-T989:4.0.3/IML74K/UVLC8:user/release-keys" PRIVATE_BUILD_DESC="SGH-T989-user 4.0.3 IML74K UVLC8 release-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-480x800
 
