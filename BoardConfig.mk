2277a8b8a998ba99ad3f3ddb7e6cc6a3d8432506
Mike Grissom <mikeyman77@gmail.com>
2012-05-31 08:25:08 -0700

    Revert "hercules: Enable QCOM_ROTATOR_KERNEL_FORMATS to fix video playback issue"
    Change-Id: I93fc96e9c3dc09da6de0b29b0a3c4ee5fc4d5416

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,9 +69,6 @@ BOARD_MTP_DEVICE := "/dev/mtp_usb"
 
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 
-# FIXME: Overlay has an issue on Hercules when playing video in landscape mode
-COMMON_GLOBAL_CFLAGS += -DQCOM_ROTATOR_KERNEL_FORMATS
-
 # Workaround for glitches while cropping bypass layers
 TARGET_NO_BYPASS_CROPPING := true
 
