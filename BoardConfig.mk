58bf247de38d831b78a36f39346981607df23340
David Ferguson <ferguson.david@gmail.com>
2012-06-07 17:04:28 -0400

    hercules: use GB kernel for recovery to avoid eMMC corruption bug
      * Added GB kernel for recovery: 2.6.35.11-I727UCKJ2-CL626051
      * Removed extraneous kernel copies from device.mk
      * Removed comically large recovery font that crept in from Note
      * Set BOARD_SUPPRESS_EMMC_WIPE to avoid issuing the dangerous
        wipe command in the first place.
    
    Since the act of testing this may brick your device, I HAVE NOT
    TESTED THIS! Without this patch though, the bad ICS leak kernel
    is used and it is sure to brick phones.
    
    Change-Id: I01113db11a6d31b671f1558b779702d92aea365c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,11 +36,13 @@ TARGET_BOOTLOADER_BOARD_NAME := hercules
 TARGET_OTA_ASSERT_DEVICE := SGH-T989,hercules
 
 # Kernel
-TARGET_PREBUILT_KERNEL      := device/samsung/hercules/prebuilt/kernel
-BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom usb_id_pin_rework=true
-BOARD_KERNEL_BASE           := 0x40400000
-BOARD_KERNEL_PAGESIZE       := 2048
-BOARD_FORCE_RAMDISK_ADDRESS := 0x41800000
+#   - Needs GB kernel for recovery since ICS kernels cause eMMC corruption
+TARGET_PREBUILT_KERNEL          := device/samsung/skyrocket/prebuilt/kernel
+TARGET_PREBUILT_RECOVERY_KERNEL := device/samsung/skyrocket/prebuilt/recovery_kernel
+BOARD_KERNEL_CMDLINE            := androidboot.hardware=qcom usb_id_pin_rework=true
+BOARD_KERNEL_BASE               := 0x40400000
+BOARD_KERNEL_PAGESIZE           := 2048
+BOARD_FORCE_RAMDISK_ADDRESS     := 0x41800000
 
 # cat /proc/emmc
 #dev:        size     erasesize name
@@ -63,6 +65,9 @@ BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Suppress the WIPE command since it can brick our EMMC
+BOARD_SUPPRESS_EMMC_WIPE := true
 
 # MTP
 BOARD_MTP_DEVICE := "/dev/mtp_usb"
