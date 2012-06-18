9796e128f7fae02b24d34a2ec259547ab39fd617
Steve Kondik <shade@chemlab.org>
2012-06-17 18:21:45 -0700

    hercules: Enable enhanced NFC
    Change-Id: Ie2f704642d01cc0207e0718c9c25e0f5708d6bac

--- cm.mk
+++ cm.mk
@@ -3,6 +3,9 @@ $(call inherit-product, device/samsung/hercules/full_hercules.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
