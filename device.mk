d69001ff6174eea478e2f2abb67756e0bc01ccbe
David Ferguson <ferguson.david@gmail.com>
2012-08-16 08:20:56 -0400

    hercules: use general name in fstab rather than device name
      * to match quincy and d2; makes diffs easier
    
    Change-Id: I2b022201e471aa0c020f2f03c68e3e9ba4cbe696

--- device.mk
+++ device.mk
@@ -42,7 +42,7 @@ PRODUCT_COPY_FILES += \
     device/samsung/hercules/ramdisk/init.target.rc:root/init.target.rc \
     device/samsung/hercules/ramdisk/ueventd.rc:root/ueventd.rc \
     device/samsung/hercules/ramdisk/init.emmc.rc:root/init.emmc.rc \
-    device/samsung/hercules/fstab.hercules:root/fstab.hercules
+    device/samsung/hercules/fstab.qcom:root/fstab.qcom
 
 # BT firmware
 PRODUCT_COPY_FILES += \
