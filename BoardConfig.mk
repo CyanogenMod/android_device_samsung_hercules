9c97570276ace6d85b007b3727c159f051a5bdba
mikeyman77 <mikeyman77@gmail.com>
2012-08-27 09:51:35 -0700

    hercules: disable LPA
    Change-Id: I4bd6cb994aef0db461788977065349700280a9b5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,8 +75,6 @@ TARGET_NO_BYPASS_CROPPING := true
 BOARD_MTP_DEVICE := "/dev/mtp_usb"
 
 # Audio
-TARGET_USES_QCOM_LPA := true
-COMMON_GLOBAL_CFLAGS += -DWITH_QCOM_LPA
 BOARD_HAS_SAMSUNG_VOLUME_BUG := true
 
 # Disable initlogo, Samsungs framebuffer is weird
