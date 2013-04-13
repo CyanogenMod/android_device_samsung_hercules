3dc8376272ec972742040a7682fc667a8b341216
Steve Kondik <shade@chemlab.org>
2013-04-12 22:21:39 -0700

    hercules: Update firmware and board name
     * Match new stock bootloader name and NFC firmware path
    
    Change-Id: Iee31046096a92e80453ed2d575c474723521752f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/samsung/hercules/BoardConfigVendor.mk
 
-TARGET_BOOTLOADER_BOARD_NAME := hercules
+TARGET_BOOTLOADER_BOARD_NAME := MSM8660_SURF
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := SGH-T989,hercules
